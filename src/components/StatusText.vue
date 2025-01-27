fcfffbb8c40e0aa7dffd49f26efdd86bf7513266
ImDarkTom <darktomg21@gmail.com>
2025-01-27 23:18:32 +0000

    🚸 ux: add usage guide

--- src/components/StatusText.vue
+++ src/components/StatusText.vue
@@ -18,6 +18,12 @@ onMounted(() => {
         })
 });
 
+function showGuide() {
+    const originUrl = window.location.origin;
+
+    alert(`Ollama does not allow external connections by default, for this app to work you need to add this URL to Ollama's trusted origins and re-launch it. You can do this by running this in command prompt or powershell: \n\n set OLLAMA_ORIGINS=\"${originUrl}\" & ollama serve`);
+}
+
 </script>
 
 <template>
@@ -25,6 +31,7 @@ onMounted(() => {
         Ollama status:
         <span ref="statusMessageElem">{{ statusMessageText }}</span>
     </span>
+    <input type="button" value="?" @click="showGuide">
 </template>
 
 <style scoped>
@@ -38,5 +45,15 @@ onMounted(() => {
 
 .status-text.fail {
     color: red;
+}
+
+input {
+    background: transparent;
+    color: var(--txt-accent);
+    font-weight: bold;
+    border: 1px solid var(--txt-accent);
+    margin-left: 1rem;
+    border-radius: 1rem;
+    cursor: pointer;
 }
 </style>
