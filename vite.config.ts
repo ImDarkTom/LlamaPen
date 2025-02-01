02a3930ac1c5cabe3c2d66e1a813ac70f5cd3c9c
ImDarkTom <darktomg21@gmail.com>
2025-02-01 21:23:45 +0000

    💚 build: fix build not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from 'node:url';
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+            "@": path.resolve(path.join(__dirname, "src/")),
+        },
     },
     css: {
         preprocessorOptions: {
             scss: {
-                additionalData: `@use '@/styles/mixins.scss' as mixin;`
-            }
-        }
-    }
+                additionalData: `@use '@/styles/mixins.scss' as mixin;`,
+            },
+        },
+    },
 });
