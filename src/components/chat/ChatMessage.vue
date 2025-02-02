9024aa97bab6c2807f81911af3d30cb1cca4afb1
ImDarkTom <darktomg21@gmail.com>
2025-02-02 03:46:28 +0000

    💄 ui: dynamic user message size

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { marked } from 'marked';
-import { BsCopy, BsPen } from 'vue-icons-plus/bs';
-import TooltipBottom from '../TooltipBottom.vue';
+import MessageOptions from './MessageOptions.vue';
 import { ref, defineProps } from 'vue';
 
-const props = defineProps<{
+defineProps<{
     message: AppMessage;
 }>();
 
@@ -33,49 +32,16 @@ marked.use({
     }]
 });
 
-function copyMessage(message: OllamaMessage) {
-    const tempElem = document.createElement('input');
-    tempElem.value = message.content;
-
-    tempElem.select();
-    tempElem.setSelectionRange(0, 99999);
-
-    navigator.clipboard.writeText(tempElem.value);
-
-    copyTooltipText.value = "Copied!";
-
-    setTimeout(() => {
-        copyTooltipText.value = "Copy text";
-    }, 1000);
-}
-
-const copyTooltipText = ref<string>("Copy text");
-function editMessage(_message: OllamaMessage) {
-    // const messageText = message.content;
-    // const messageIndex = chatStore.messages.findIndex((storedMessage) => storedMessage === message);
-
-    // chatStore.messages.splice(messageIndex);
-
-    console.log("to be added");
-}
-
 </script>
 
 <template>
-    <div class="chat-message" :class="{ 'bubble': props.message.role === 'user', 'full': props.message.role === 'assistant' }">
-        <span class="message-creator">{{ props.message.role }}</span>
+    <div class="chat-message" :class="{ 'bubble': message.role === 'user', 'full': message.role === 'assistant' }">
+        <!-- <span class="message-creator">{{ message.role }}</span> -->
 
-        <span v-if="props.message.role !== 'user'" v-html="marked(props.message.content)" class="message-text"></span>
-        <span v-else v-html="props.message.content" class="message-text"></span>
+        <span v-if="message.role !== 'user'" v-html="marked(message.content)" class="message-text"></span>
+        <span v-else v-html="message.content" class="message-text"></span>
 
-        <div class="message-options">
-            <TooltipBottom v-if="props.message.role !== 'user'" :text="copyTooltipText" >
-                <BsCopy title="Copy Text" @click="copyMessage(props.message)" />
-            </TooltipBottom>
-            <TooltipBottom v-else text="Edit" >
-                <BsPen title="Edit" @click="editMessage(props.message)" />
-            </TooltipBottom>
-        </div>
+        <MessageOptions :message="message" />
     </div>
 </template>
 
@@ -94,24 +60,14 @@ function editMessage(_message: OllamaMessage) {
         margin-left: auto;
         border-radius: 1rem;
         background-color: var(--bg-3);
-        width: clamp(350px, 50%, 1280px);
+        max-width: 28rem;
+        min-width: fit-content;
     }
 
     &.full {
         width: 100%;
         box-sizing: border-box;
         margin: 0;
-
-        .message-creator {
-            display: none;
-        }
-    }
-
-    .message-creator {
-        font-weight: bold;
-        text-transform: capitalize;
-        padding-bottom: 0.8rem;
-        font-size: 1.2rem;
     }
 
     .message-text {
@@ -124,26 +80,6 @@ function editMessage(_message: OllamaMessage) {
             background-color: rgba($color: #000000, $alpha: 0.5);
             box-sizing: border-box;
             padding: 0.5rem;
-        }
-    }
-
-    .message-options {
-        display: flex;
-        flex-direction: row;
-        gap: 0.5rem;
-        padding-top: 1rem;
-
-        > * {
-            box-sizing: content-box;
-            padding: 0.5rem;
-            border-radius: 0.5rem;
-            cursor: pointer;
-
-            &:hover {
-                @include mixin.shadow-low;
-
-                background-color: var(--bg-2);
-            }
         }
     }
 }
---
+++ src/components/chat/MessageOptions.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import TooltipBottom from '../TooltipBottom.vue';
+import { BsCopy, BsPen } from 'vue-icons-plus/bs';
+
+defineProps<{
+    message: AppMessage,
+}>();
+
+function copyMessage(message: OllamaMessage) {
+    const tempElem = document.createElement('input');
+    tempElem.value = message.content;
+
+    tempElem.select();
+    tempElem.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(tempElem.value);
+
+    copyTooltipText.value = "Copied!";
+
+    setTimeout(() => {
+        copyTooltipText.value = "Copy text";
+    }, 1000);
+}
+
+const copyTooltipText = ref<string>("Copy text");
+function editMessage(_message: OllamaMessage) {
+    // const messageText = message.content;
+    // const messageIndex = chatStore.messages.findIndex((storedMessage) => storedMessage === message);
+
+    // chatStore.messages.splice(messageIndex);
+
+    console.log("to be added");
+}
+</script>
+
+<template>
+    <div class="message-options">
+        <TooltipBottom v-if="message.role !== 'user'" :text="copyTooltipText">
+            <BsCopy title="Copy Text" @click="copyMessage(message)" />
+        </TooltipBottom>
+        <TooltipBottom v-else text="Edit">
+            <BsPen title="Edit" @click="editMessage(message)" />
+        </TooltipBottom>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.message-options {
+    display: flex;
+    flex-direction: row;
+    gap: 0.5rem;
+    padding-top: 1rem;
+
+    >* {
+        box-sizing: content-box;
+        padding: 0.5rem;
+        border-radius: 0.5rem;
+        cursor: pointer;
+
+        &:hover {
+            @include mixin.shadow-low;
+
+            background-color: var(--bg-2);
+        }
+    }
+}
+</style>
