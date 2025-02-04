c0c0bcc59b8ca8a49c9af6fe46860cd1f2f4035b
ImDarkTom <darktomg21@gmail.com>
2025-02-04 02:32:05 +0000

    ⚡️ performance: use config store for custom url

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,6 @@
 <script setup lang="ts">
 import ChatWindow from './ChatWindow.vue';
 import Sidebar from './sidebar/Sidebar.vue';
-
-if (!localStorage.getItem('customUrl')) {
-    localStorage.setItem('customUrl', 'http://localhost:11434');
-    console.log("Set custom URL to default on first load.");
-}
-
-// TODO:
-// Thinking timer
-// Generated message options:
-//   - Regenerate
-//   - Cancel generation (while generating)
-//   - Like/dislike button, perhaps by using the system role and sending a message saying the user liked/disliked the output(?)
-// Fix parsing LaTeX and blocks from deepseek-r1 models
-// Learn how to organise component files
-// Settings UI (far, far later)
-
 </script>
 
 <template>
