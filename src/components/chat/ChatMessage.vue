cf074f4a0542405594845ce8746846335600997f
ImDarkTom <darktomg21@gmail.com>
2025-02-10 10:16:07 +0000

    ♻️ refactor: refactor ChatMessage to tailwind

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -86,9 +86,9 @@ function finishEdit(newText: string) {
 </script>
 
 <template>
-    <div class="chat-message" :class="{ 
-        'bubble': props.message.role === 'user' && !editing, 
-        'full': props.message.role === 'assistant' || editing 
+    <div class="text-txt-1 box-border p-4 m-4 flex flex-col whitespace-pre-wrap" :class="{ 
+        'ml-auto rounded-xl bg-primary-300 max-w-[70%] shadow-sm shadow-black': props.message.role === 'user' && !editing, 
+        'w-full box-border !p-0 !m-0': props.message.role === 'assistant' || editing 
     }">
         <MessageEditor v-if="editing" 
             ref="messageEditorRef" 
@@ -96,7 +96,7 @@ function finishEdit(newText: string) {
             @onCancelEdit="cancelEditing" 
             @onFinishEditing="finishEdit" 
         />
-        
+
         <template v-else>
             <span v-if="message.role !== 'user'" v-html="marked.parse(message.content)" class="message-text"></span>
             <div v-else class="message-text">{{ message.content }}</div>
@@ -106,58 +106,15 @@ function finishEdit(newText: string) {
     </div>
 </template>
 
-<style scoped lang="scss">
-.chat-message {
-    color: var(--txt-1);
-    box-sizing: border-box;
-    padding: 1rem;
-    margin: 1rem;
-    display: flex;
-    flex-direction: column;
-    white-space: pre-wrap;
+<style>
+@reference "@/styles/style.css";
 
-    &.bubble {
-        box-shadow: 0px 3px 10px -3px black;
-
-        margin-left: auto;
-        border-radius: 1rem;
-        background-color: var(--bg-3);
-        max-width: 70%;
-    }
-
-    &.full {
-        width: 100%;
-        box-sizing: border-box;
-        margin: 0;
-    }
-
-    .message-text {
-        pre {
-            overflow-x: auto;
-        }
-    }
-}
-</style>
-
-<style lang="scss">
 .thought-text-container,
 think {
-    font-style: italic;
-    filter: opacity(0.8);
-    padding-bottom: 1rem;
-    display: flex;
-    flex-direction: column;
-    background-color: var(--bg-3);
-    border-radius: 1rem;
-    padding: 0.5rem;
-    box-sizing: border-box;
-    margin-bottom: 1rem;
+    @apply italic opacity-80 pb-4 flex bg-primary-200 rounded-xl p-2 box-border mb-4;
+}
 
-    .thought-header {
-        font-weight: bold;
-        padding: 0.5rem 0;
-        font-style: normal;
-        filter: none;
-    }
+.message-text pre {
+    @apply overflow-x-auto mb-4 border border-txt-1/50 rounded-lg;
 }
 </style>
