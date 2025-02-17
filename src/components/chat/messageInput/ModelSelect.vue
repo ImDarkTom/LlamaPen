18afd5b330bea47d864a6d56c1ba6d6d35cde261
ImDarkTom <darktomg21@gmail.com>
2025-02-17 22:43:07 +0000

    🎨 format: organise and optimise ModelSelect

--- src/components/chat/messageInput/ModelSelect.vue
+++ src/components/chat/messageInput/ModelSelect.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
-import { computed, nextTick, onMounted, ref } from 'vue';
+import { computed, nextTick, onBeforeUnmount, onMounted, ref } from 'vue';
 import { useConfigStore } from '../../../stores/config';
-import { BsChevronDown } from 'vue-icons-plus/bs';
+import { BsChevronDown, BsChevronUp } from 'vue-icons-plus/bs';
+import errorHandler from '../../../utils/errorHandler';
 
 const config = useConfigStore();
 
+// State
 const modelsList = ref<ModelList>([]);
 const selectedModel = ref<string | null>(localStorage.getItem('selectedModel'));
 
+// UI State
+const showSelect = ref<boolean>(false);
+const searchQuery = defineModel<string>('');
+const searchFocused = ref<boolean>(false);
+const focusedItemIndex = ref<number>(0);
+
+// Refs
+const searchBarRef = ref<HTMLInputElement | null>(null);
+const listItemsRef = ref<Array<HTMLElement | null>>([]);
+
+// Lifecycle hooks
 onMounted(() => {
     fetch(config.apiUrl('/api/tags'))
         .then(response => response.json())
@@ -17,86 +30,116 @@ onMounted(() => {
             if (!selectedModel.value || !modelsList.value.map((item) => item.name).includes(selectedModel.value)) {
                 selectedModel.value = response.models[0].model;
             }
+        })
+        .catch((error) => {
+            errorHandler.handleError(error, `Unable to connect to Ollama at ${config.ollamaUrl}`, true);
         });
+
+    document.addEventListener('keydown', handleKeyboardShortcuts)
 });
+
+onBeforeUnmount(() => {
+    document.removeEventListener('keydown', handleKeyboardShortcuts);
+});
+
+// Computed properties
+const queriedModelList = computed<ModelList>(() => {
+    return modelsList.value.filter((model) => model.name.toLowerCase().includes((searchQuery.value || "").toLowerCase()));
+});
+
+// Functions
+function handleKeyboardShortcuts(e: KeyboardEvent) {
+    if (e.key === "M" && e.ctrlKey && e.shiftKey) {
+        e.preventDefault();
+        toggleShowSelect();
+    }
+}
 
 function setModel(newModelName: string) {
     selectedModel.value = newModelName;
     localStorage.setItem('selectedModel', newModelName);
     toggleShowSelect();
     searchQuery.value = "";
+}
+
+function resetState() {
+    searchQuery.value = "";
     focusedItemIndex.value = 0;
 }
 
-const showSelect = ref<boolean>(false);
-const searchBarRef = ref<HTMLInputElement | null>(null);
-
-async function toggleShowSelect() {
+function toggleShowSelect() {
     showSelect.value = !showSelect.value;
 
     if (showSelect) {
-        await nextTick();
-        searchBarRef.value?.focus();
+        nextTick(() => {
+            searchBarRef.value?.focus();
+            listItemsRef.value[focusedItemIndex.value]?.scrollIntoView({ block: 'center' });
+        });
     }
 }
 
 function searchKeyDown(e: KeyboardEvent) {
-    if (e.key === "Enter") {
-        setModel(queriedModelList.value[focusedItemIndex.value].name);
-        return;
+    let scrollDown = false;
+    switch (e.key) {
+        case "Enter":
+            setModel(queriedModelList.value[focusedItemIndex.value].name);
+            break;
+
+        case "Escape":
+            toggleShowSelect();
+            resetState();
+            break;
+
+        case "ArrowUp":
+            focusedItemIndex.value = Math.max(focusedItemIndex.value - 1, 0); // back 1 index or keep at 0
+            scrollDown = true;
+            break;
+
+        case "ArrowDown":
+            focusedItemIndex.value = Math.min(focusedItemIndex.value + 1, queriedModelList.value.length - 1); // up 1 index or keep at max
+            scrollDown = true;
+            break;
     }
 
-    if (e.key === "Escape") {
-        toggleShowSelect();
-        return
+    if (scrollDown) {
+        nextTick(() => {
+            listItemsRef.value[focusedItemIndex.value]?.scrollIntoView({ block: 'center' });
+        });
     }
-
-    if (e.key === "ArrowUp") {
-        focusedItemIndex.value = Math.max(focusedItemIndex.value - 1, 0); // back 1 index or keep at 0
-    }
-
-    if (e.key === "ArrowDown") {
-        focusedItemIndex.value = Math.min(focusedItemIndex.value + 1, queriedModelList.value.length - 1); // back 1 index or keep at 0
-    }
-
-    nextTick(() => {
-        listItemsRef.value[focusedItemIndex.value]?.scrollIntoView({ block: 'nearest' });
-    });
 }
-
-const modelListRef = ref<HTMLElement | null>(null);
-const listItemsRef = ref<Array<HTMLElement | null>>([]);
-
-const searchQuery = defineModel<string>('');
-
-const queriedModelList = computed<ModelList>(() => {
-    return modelsList.value.filter((model) => model.name.toLowerCase().includes((searchQuery.value || "").toLowerCase()));
-});
-
-const searchFocused = ref<boolean>(false);
-const focusedItemIndex = ref<number>(0);
-
 </script>
 
 <template>
     <div class="relative">
         <div @click="toggleShowSelect"
-            class="bg-primary-400 hover:bg-primary-500 cursor-pointer p-3 box-border rounded-xl flex flex-row gap-2 text-txt-2 hover:text-txt-1 transition-colors duration-150 select-none">
+            class="bg-primary-400 hover:bg-primary-500 cursor-pointer p-3 box-border rounded-xl flex flex-row gap-2 text-txt-2 hover:text-txt-1 transition-colors duration-150 select-none"
+            aria-haspopup="listbox"
+            :aria-expanded="showSelect"
+            >
             {{ selectedModel }}
-            <BsChevronDown class="w-4 h-full" />
+            <BsChevronUp v-if="showSelect" class="w-4 h-full" />
+            <BsChevronDown v-else class="w-4 h-full" />
         </div>
-        <div v-if="showSelect" class="absolute left-0 bottom-full mb-2 bg-primary-300 p-1 rounded-xl">
-            <input ref="searchBarRef" v-model="searchQuery" @focus="searchFocused = true" @blur="searchFocused = false" @keydown="searchKeyDown" type="search" placeholder="Search a model..." class="bg-primary-400 focus:bg-primary-500 w-full rounded-xl h-10 p-2 !mb-1 outline-0">
-            <ul ref="modelListRef" role="list" class="max-h-80 overflow-y-auto">
-                <li role="listitem" @click="setModel(model.name)" v-for="(model, index) in queriedModelList" :key="model.name"
+
+        <div v-if="showSelect" class="absolute left-0 bottom-full mb-2 bg-primary-300 p-1 rounded-xl" role="listbox">
+            <input ref="searchBarRef" v-model="searchQuery" @focus="searchFocused = true" @blur="searchFocused = false" @keydown="searchKeyDown" type="search" placeholder="Search a model..." 
+                class="bg-primary-400 focus:bg-primary-500 w-full rounded-xl h-10 p-2 !mb-1 outline-0"
+                aria-label="Search for a model..."
+                aria-controls="model-list">
+            
+            <ul role="list" class="max-h-80 overflow-y-auto">
+                <li role="listitem" v-for="(model, index) in queriedModelList" :key="model.name"
                     class="flex flex-col cursor-pointer px-3 py-2 hover:bg-primary-400 rounded-xl"
                     :class="{ 'bg-primary-500': index === focusedItemIndex }"
+                    @click="setModel(model.name)"
                     ref="listItemsRef"
+                    :aria-selected="index === focusedItemIndex"
                     >
                     <span class="min-w-max text-md font-semibold">{{ model.name }}</span>
                     <span class="text-sm text-txt-2">{{ model.details.parameter_size }}</span>
                 </li>
             </ul>
+
         </div>
     </div>
 </template>
