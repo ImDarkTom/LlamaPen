67cd2c480619e2f82bca7a131f2d411ce34d3429
ImDarkTom <darktomg21@gmail.com>
2025-02-19 11:08:00 +0000

    🎨 format: add title to ollama status text

--- src/components/sidebar/footer/StatusText.vue
+++ src/components/sidebar/footer/StatusText.vue
@@ -4,13 +4,22 @@ import { useConfigStore } from '../../../stores/config';
 
 const config = useConfigStore();
 
+// UI State
 const statusMessageText = ref("Waiting for Ollama...");
-const statusMessageElem = ref<HTMLElement | null>(null);
 const connectedToOllama = ref<boolean | null>(null);
+
+// Refs
+
+const statusMessageElem = ref<HTMLElement | null>(null);
 
 onMounted(() => {
     fetch(config.apiUrl('/'))
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP Error when connecting to Ollama, code: ${response.status}`)
+            }
+            return response.text();
+        })
         .then(response => {
             statusMessageText.value = response;
             connectedToOllama.value = true;
@@ -27,7 +36,7 @@ onMounted(() => {
     <div class="overflow-hidden overflow-ellipsis py-2">
         <span class="text-amber-400" :class="{ 'text-emerald-400': connectedToOllama === true, 'text-red-400': connectedToOllama === false }">
             Ollama status:
-            <span ref="statusMessageElem">{{ statusMessageText }}</span>
+            <span ref="statusMessageElem" :title="statusMessageText">{{ statusMessageText }}</span>
         </span>
     </div>
 </template>
