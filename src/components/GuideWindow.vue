2c4bd05fe68094481d247a8b973e237d2cba03c8
ImDarkTom <darktomg21@gmail.com>
2025-02-20 23:16:22 +0000

    📝 docs: update wording in setup guide

--- src/components/GuideWindow.vue
+++ src/components/GuideWindow.vue
@@ -25,13 +25,13 @@ onMounted(() => {
             <br>
 
             <p>or, if you want to be able to connect every time without re-running this command each time, you can instead run
-                this to persistently add this URL to Ollama's trusted origins: </p>
+                this to persistently add this app URL to Ollama's trusted origins: </p>
             <br>
 
             <pre class="my-2"><code class="hljs language-bash !my-2">setx OLLAMA_ORIGINS="{{ originUrl }}"</code></pre>
             <br>
 
-            <p>After this, just open Ollama normally and you should be able to access it via this website.</p>
+            <p>After that, just open Ollama normally and you should be able to connect after refreshing this page.</p>
 
             <h3 class="text-2xl pb-2 pt-4 flex flex-row items-center"><AiOutlineLinux />/<AiFillApple />&nbsp;Linux/MacOS </h3>
             <p>On linux or MacOS, you can run a similar command:</p>
@@ -50,7 +50,7 @@ onMounted(() => {
             <h2 class="text-3xl font-semibold pb-2">Troubleshooting</h2>
             
             <h3 class="text-2xl flex flex-row items-center">Common Issues</h3>
-            <h4 class="text-xl pb-2 pt-4 flex flex-row items-center">Error: listen tcp &#60;ip&#62;:&#60;port&#62;: bind: Only one usage of each socket address (protocol/network address/port) is normally permitted.</h4>
+            <h4 class="text-xl pb-2 pt-4 flex flex-row items-center italic">Error: listen tcp &#60;ip&#62;:&#60;port&#62;: bind: Only one usage of each socket address (protocol/network address/port) is normally permitted.</h4>
 
             <p>The most common cause for this error is that Ollama is already running. Make sure to close Ollama/end the process before trying again.
                 <br><br>
