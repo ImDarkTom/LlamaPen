b0d5e3258f59bbf50aa4fa29607fc9a194b31bcf
ImDarkTom <darktomg21@gmail.com>
2025-02-25 23:56:54 +0000

    🚸 ux: refresh page on ollama url change

--- src/components/sidebar/footer/CustomUrlButton.vue
+++ src/components/sidebar/footer/CustomUrlButton.vue
@@ -7,12 +7,17 @@ function customUrlDialog() {
     const currentUrl = config.ollamaUrl;
     const customUrl = prompt("Enter a custom URL to use as a Ollama instance, write in the format of an origin i.e. 'http://example.com:8080'. \n\n Leave blank to reset to default (http://localhost:11434): ", currentUrl);
 
+    if (customUrl === currentUrl) {
+        return;
+    }
+
     if (!customUrl || customUrl == "") {
         config.setOllamaUrl('http://localhost:11434')
         return;
     }
 
-    config.setOllamaUrl(customUrl);;
+    config.setOllamaUrl(customUrl);
+    location.reload();
 }
 </script>
 
