029408e6991aa0c6d83fbde176c12a0f8c32e60e
ImDarkTom <darktomg21@gmail.com>
2025-02-26 16:29:59 +0000

    🔥 remove: remove useless focus detect on textarea container

--- src/components/TextpadWindow.vue
+++ src/components/TextpadWindow.vue
@@ -31,8 +31,9 @@ function loadTextpad() {
 
 	language.value = allTextpadsStore.openedTextpad?.language || "plaintext";
 
-	mainTextarea.value.value = allTextpadsStore.openedTextpad?.content || "";
+	mainTextarea.value.value = allTextpadsStore.openedTextpad?.content || '';
 	updateVirtualTextArea();
+	mainTextarea.value.focus();
 
 	uiStore.setLastOpenedTextpad(route.params.id as string);
 }
@@ -82,14 +83,14 @@ function handleChangeLanguage(e: Event) {
 <template>
     <div class="w-full h-full flex flex-col p-2 box-border">
         <div class="pl-14 h-14 flex flex-row gap-1 bg-primary-400 mb-2 p-1 rounded-lg box-border">
-			<select @change="handleChangeLanguage" v-model="language" class="h-full hover:bg-primary-500 p-2 rounded-lg text-txt-2 focus:text-txt-1 cursor-pointer">
+			<select @change="handleChangeLanguage" v-model="language" class="h-full hover:bg-primary-500 p-2 rounded-lg text-txt-2 	focus:text-txt-1 cursor-pointer">
 				<option value="plaintext">Plaintext</option>
 				<option value="html">HTML</option>
 				<option value="js">JavaScript</option>
 			</select>
 			<ModelSelect direction="down" />
 		</div>
-		<div class="grow size-full p-4 dot-bg rounded-lg border-[1px] border-txt-2/50 shadow-md shadow-black focus:border-txt-2
+		<div class="grow size-full p-4 dot-bg rounded-lg border-[1px] border-txt-2/50 shadow-md shadow-black group
 				bg-radial from-txt-2/25 via-primary-500 via-[2px] to-primary-500 bg-[length:2rem_2rem] bg-[position:-1rem_-1rem]">
 			<div class="relative size-full">
 				<!-- no clue why we need py-1 to have it align, try properly fix it later -->
