2e1f37531d9cd4a9817f56484c236c541b0871a5
ImDarkTom <darktomg21@gmail.com>
2025-02-28 08:59:41 +0000

    💄 ui: improve popup appearance

--- src/components/ui/Popup.vue
+++ src/components/ui/Popup.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onBeforeUnmount, onMounted, ref } from 'vue';
 import { emitter, PopupButtons } from '../../mitt';
+import { BiSolidErrorCircle } from 'vue-icons-plus/bi';
 
 const showing = ref<boolean>(false);
 const title = ref<string>('Popup');
@@ -36,15 +37,17 @@ function close() {
 </script>
 
 <template>
-    <div class="absolute top-0 left-0 w-full h-full bg-black/50" 
+    <div class="absolute top-0 left-0 w-full h-full bg-black/50
+        motion-opacity-in-[75%] motion-duration-[0.1s]" 
         :class="{ 'hidden': !showing }"
         aria-labelledby="popupTitle"
         aria-describedby="popupText">
-        <div class="flex flex-col w-full sm:w-[70%] lg:w-[50%] h-[60%] rounded-xl p-6 box-border bg-primary-300 absolute top-[50%] left-[50%] -translate-[50%]">
-            <h2 id="popupTitle" class="text-3xl font-semibold">{{ title }}</h2>
-            <p id="popupText" class="mt-4 grow">{{ message }}</p>
+        <div class="flex flex-col w-full sm:w-[70%] lg:w-[50%] h-[60%] rounded-xl p-6 box-border bg-gradient-to-br from-primary-300 to-primary-400 absolute top-[50%] left-[50%] -translate-[50%] shadow-md shadow-black border-[1px] border-txt-2/50
+            motion-scale-in-[0.75] motion-opacity-in-[75%] motion-blur-in-[2px] motion-duration-[0.1s]">
+            <h2 id="popupTitle" class="text-3xl font-semibold flex flex-row items-center gap-2"><BiSolidErrorCircle class="h-full w-auto" />{{ title }}</h2>
+            <p id="popupText" class="mt-4 grow text-lg line-space">{{ message }}</p>
             <div>
-                <button class="mt-4 bg-primary-400 w-36 h-12 rounded-xl cursor-pointer hover:font-semibold" @click="close">Close</button>
+                <button class="mt-4 bg-txt-2 text-primary-400 font-semibold hover:shadow-sm shadow-black/50 hover:scale-105 transition-all duration-100 w-36 h-12 rounded-xl cursor-pointer" @click="close">Close</button>
             </div>
         </div>
     </div>
