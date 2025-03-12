6a3b39397b72242f84d177fd81407096232a2ea4
ImDarkTom <darktomg21@gmail.com>
2025-03-12 00:45:17 +0000

    🐛 bug: fix SidebarRouterLink not compiling

--- src/components/Sidebar/SidebarRouterLink.vue
+++ src/components/Sidebar/SidebarRouterLink.vue
@@ -1,9 +1,10 @@
-<!-- Source: https://router.vuejs.org/guide/advanced/extending-router-link.html -->
-<script setup>
+<script setup lang="ts">
+// https://router.vuejs.org/guide/advanced/extending-router-link.html
+
 import { emitter } from '@/mitt';
 import { useConfigStore } from '@/stores/config';
-import { computed } from 'vue'
-import { RouterLink } from 'vue-router'
+import { computed, type PropType } from 'vue'
+import { RouterLink, type NavigationFailure, type RouteLocationPathRaw } from 'vue-router'
 
 const config = useConfigStore();
 
@@ -11,9 +12,19 @@ defineOptions({
 	inheritAttrs: false,
 })
 
+defineSlots<{
+	default: void,
+	isActive: Boolean,
+}>()
+
 const props = defineProps({
-	// add @ts-ignore if using TypeScript
+	// @ts-ignore
 	...RouterLink.props,
+	to: {
+        type: Object as PropType<RouteLocationPathRaw>,
+        required: true
+    },
+	activeClass: String,
 	inactiveClass: String,
 })
 
@@ -21,7 +32,7 @@ const isExternalLink = computed(() => {
 	return typeof props.to === 'string' && props.to.startsWith('http')
 });
 
-const handleNavigate = (navigate) => {
+const handleNavigate = (navigate: (e?: MouseEvent) => Promise<void | NavigationFailure>) => {
 	if (window.innerWidth <= 384 && config.closeSidebarOnNavMobile) {
         console.log("hiding sidebar");
         emitter.emit('hideSidebar');
@@ -33,12 +44,18 @@ const handleNavigate = (navigate) => {
 </script>
 
 <template>
-	<a v-if="isExternalLink" v-bind="$attrs" :href="to" target="_blank">
-		<slot />
+	<a v-if="isExternalLink" v-bind="$attrs" :href="props.to" target="_blank">
+		<slot></slot>
 	</a>
-	<router-link v-else v-bind="$props" custom v-slot="{ isActive, href, navigate }">
-		<a v-bind="$attrs" :href="href" @click.prevent @mousedown.prevent="handleNavigate(navigate)" :class="isActive ? activeClass : inactiveClass">
-			<slot />
+	<router-link v-else to="" v-bind="$props" custom v-slot="{ isActive, href, navigate }">
+		<a 
+			v-bind="$attrs" 
+			:href="href" 
+			@click.prevent 
+			@mousedown.prevent="handleNavigate(navigate)" 
+			:class="isActive ? props.activeClass : props.inactiveClass"
+		>
+			<slot></slot>
 		</a>
 	</router-link>
 </template>
