254c834f8cb5ca9c2e7ca07779322a80ab5883ca
ImDarkTom <darktomg21@gmail.com>
2025-03-16 02:36:05 +0000

    🐛 bug: fix sidebar toggle button not being on z-index

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -65,7 +65,7 @@ onUnmounted(() => {
                 <SidebarOptions />
             </div>
         </Transition>
-        <div class="absolute top-0 right-0 transform translate-x-full h-12 w-12 p-1">
+        <div class="absolute top-0 right-0 transform translate-x-full h-12 w-12 p-1 z-30">
             <div class="h-10 w-10 p-1 cursor-pointer rounded-lg hover:bg-primary-300 hover:shadow-md shadow-black/50 transition-all duration-100"
                 @pointerdown="handlePointerDown" aria-label="Toggle Sidebar" :aria-expanded="showSidebar">
                 <TbLayoutSidebarLeftCollapse class="size-full" v-if="showSidebar" />
