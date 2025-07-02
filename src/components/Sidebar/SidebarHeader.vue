004c2f7caa6fcd0868ba04d5e05e1a90268acd84
ImDarkTom <darktomg21@gmail.com>
2025-07-02 03:46:02 +0100

    fix: use correct color for app logo

--- src/components/Sidebar/SidebarHeader.vue
+++ src/components/Sidebar/SidebarHeader.vue
@@ -2,6 +2,9 @@
 import { AiOutlineSearch } from 'vue-icons-plus/ai';
 import SidebarRouterLink from './SidebarRouterLink.vue';
 
+const logoFile = import.meta.glob('@/icons/llamapen/favicon.svg', { eager: true });
+const logo = Object.values(logoFile)[0];
+
 function search() {
     alert('to be added...');
 }
@@ -13,7 +16,7 @@ function search() {
             <div class="size-10 p-1"></div><!-- sidebar toggle space holder -->
             <SidebarRouterLink to="/"
                 class="max-h-10 w-1/2 flex justify-center items-center hover:brightness-75 hover:scale-90 active:scale-110 transition-all duration-100">
-                <img src="/favicon.svg" alt="LlamaPen Logo" class="size-10 p-1" />
+                <logo alt="LlamaPen Logo" class="size-10 p-1 text-text" />
             </SidebarRouterLink>
             <AiOutlineSearch aria-label="Search"
                 class="h-6 w-auto cursor-pointer p-2 box-content rounded-lg text-text hover:bg-background-light hover:shadow-xs shadow-black/50"
