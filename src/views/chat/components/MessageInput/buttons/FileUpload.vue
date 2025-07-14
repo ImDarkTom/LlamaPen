bce4cec9ebcaf8056c74b7f829946d1b3b19e047
ImDarkTom <darktomg21@gmail.com>
2025-07-14 17:55:08 +0300

    feat: hide file upload if model does not have vision

--- src/views/chat/components/MessageInput/buttons/FileUpload.vue
+++ src/views/chat/components/MessageInput/buttons/FileUpload.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-import { AiOutlinePlus } from 'vue-icons-plus/ai';
+import { useUiStore } from '@/stores/uiStore';
+import { computed } from 'vue';
+import { MdOutlineAddPhotoAlternate } from 'vue-icons-plus/md';
+
+const uiStore = useUiStore();
 
 defineProps<{
     onChange: (event: Event) => void
 }>();
+
+const selectedModelHasVision = computed(() => {
+    return uiStore.chat.selectedModelInfo?.capabilities.includes('vision');
+});
+
+function onClick(e: MouseEvent) {
+    if (!selectedModelHasVision.value) {
+        e.preventDefault();
+        return;
+    }
+}
 </script>
 
 <template>
-    <div class="aspect-square msg-input-secondary-btn !p-0"
-        title="Upload file(s)">
-        <label for="file-upload" class="cursor-pointer size-full flex items-center justify-center">
-            <AiOutlinePlus />
+    <div 
+        class="aspect-square msg-input-secondary-btn !p-0"
+        :class="{ 'opacity-50': !selectedModelHasVision }"
+        :title="selectedModelHasVision ? 'Upload file(s)' : 'Selected model does not have vision capabilities'"
+    >
+        <label 
+            for="file-upload" 
+            class="cursor-pointer size-full flex items-center justify-center"
+        >
+            <MdOutlineAddPhotoAlternate />
         </label>
-        <input type="file" id="file-upload" class="hidden" accept="image/*" multiple @change="onChange" />
+        <input type="file" id="file-upload" class="hidden" accept="image/*" multiple @change="onChange" @click="onClick" />
     </div>
 </template>
