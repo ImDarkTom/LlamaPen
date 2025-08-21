d18d50bf998b949b08844324964be3cc269ea9ee
ImDarkTom <darktomg21@gmail.com>
2025-08-21 01:47:09 +0100

    chore: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// eslint.config.mjs
+import pluginVue from 'eslint-plugin-vue'
+import {
+    defineConfigWithVueTs,
+    vueTsConfigs,
+    configureVueProject,
+} from '@vue/eslint-config-typescript'
+
+configureVueProject({
+    tsSyntaxInTemplates: true,
+    scriptLangs: [
+        'ts',
+    ],
+    allowComponentTypeUnsafety: true,
+    rootDir: import.meta.dirname,
+})
+
+export default defineConfigWithVueTs(
+    pluginVue.configs["flat/essential"],
+
+    vueTsConfigs.recommended,
+)
