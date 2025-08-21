e8a15cdaf8271281c406e59820a5a93a0e77f810
ImDarkTom <darktomg21@gmail.com>
2025-08-21 03:24:18 +0100

    chore: eslint ignore unused vars starting with underscore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,13 @@ configureVueProject({
 
 export default defineConfigWithVueTs(
     pluginVue.configs["flat/essential"],
-
     vueTsConfigs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+            ],
+        }
+    }
 )
