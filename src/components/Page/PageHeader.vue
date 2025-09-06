4c3edaae37a44d6d6d8e29faffa3678054855e17
ImDarkTom <darktomg21@gmail.com>
2025-09-06 02:38:57 +0100

    refactor: simplify pageheader component

--- src/components/Page/PageHeader.vue
+++ src/components/Page/PageHeader.vue
@@ -9,13 +9,13 @@ defineProps<{
 </script>
 
 <template>
-    <div class="relative w-full flex flex-row justify-between items-center">
-            <h1 class="text-4xl font-extrabold mt-2 pr-3 bg-background text-text">{{ text }}</h1>
-            <div class="w-full h-0.5 bg-text absolute top-1/2 translate-y-1/2 -z-1 rounded-full"></div>
-            <div class="bg-background pl-2 box-border">
-                <AiOutlineClose
-                    class="size-10 hover:bg-background-light text-text hover:text-text-muted cursor-pointer rounded-full p-1 transition-colors duration-dynamic"
-                    @click="router.back()" />
-            </div>
+    <div class="flex items-center text-text">
+        <h1 class="text-4xl font-extrabold pr-3">{{ text }}</h1>
+        <div class="grow h-0.5 bg-text"></div>
+        <div class="bg-background pl-2 box-border">
+            <AiOutlineClose
+                class="size-10 hover:bg-background-light hover:text-text-muted cursor-pointer rounded-full p-1 transition-colors duration-dynamic hover:duration-0"
+                @click="router.back()" />
         </div>
+    </div>
 </template>
