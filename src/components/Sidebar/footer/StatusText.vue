b658130519c74a3197fe73e2650497d137f3a53f
ImDarkTom <darktomg21@gmail.com>
2025-10-06 16:41:49 +0100

    feat: update ollama status indicator

--- src/components/Sidebar/footer/StatusText.vue
+++ src/components/Sidebar/footer/StatusText.vue
@@ -2,39 +2,60 @@
 import { onMounted, ref } from 'vue';
 import { emitter } from '../../../lib/mitt';
 import { useModelList } from '@/composables/useModelList';
+import OllamaIcon from '@/components/Icon/OllamaIcon.vue';
+import { BiChevronDown } from 'vue-icons-plus/bi';
 
 const { connectedToOllama, load, error, loading } = useModelList();
 
 // UI State
 const statusMessageText = ref("Waiting for Ollama...");
+const moreInfoText = ref('Attempting to connect to Ollama...');
 
 onMounted(async () => {
     await load();
 
     if (connectedToOllama.value) {
-        statusMessageText.value = "Ollama is running";
+        statusMessageText.value = "Connected";
+        moreInfoText.value = "Connected to Ollama!";
     } else if (error) {
         if (error.value === "NetworkError when attempting to fetch resource.") {
-            statusMessageText.value = "Network error, is Ollama running?";
+            statusMessageText.value = "Disconnected";
+            moreInfoText.value = "Network error, is Ollama running?";
         } else {
-            statusMessageText.value = error.value || 'Unknown Error';
+            statusMessageText.value = "Unknown Error";
+            moreInfoText.value = error.value || 'Unknown Error';
         }
 
         emitter.emit('openNotConnectedPopup');
     }
 });
+
+const expanded = ref(false);
+function toggle() {
+    expanded.value = !expanded.value;
+}
 </script>
 
 <template>
-    <div class="overflow-hidden overflow-ellipsis font-semibold">
-        <span
-            :class="{ 
-                'text-warning': loading,
-                'text-success': connectedToOllama && !loading, 
-                'text-danger': !connectedToOllama && !loading
-                }">
-            Ollama status:
-            <span class="font-normal" :title="statusMessageText">{{ statusMessageText }}</span>
-        </span>
+    <div 
+        class="overflow-hidden overflow-ellipsis p-1 rounded-md ring-1 cursor-pointer transition-colors duration-dynamic"
+        :class="{ 
+            'text-warning bg-warning/25 hover:bg-warning/35 ring-warning/50': loading,
+            'text-success bg-success/25 hover:bg-success/35 ring-success/50': connectedToOllama && !loading, 
+            'text-danger bg-danger/25 hover:bg-danger/35 ring-danger/50': !connectedToOllama && !loading
+        }"
+        @click="toggle">
+        <div class="flex flex-row gap-2">
+            <OllamaIcon class="inline size-6" />
+            <span 
+                class="font-medium" 
+                :class="{ 'animate-pulse': loading }"
+                :title="statusMessageText"
+                >{{ statusMessageText }}</span>
+            <BiChevronDown class="inline ml-auto transition-transform duration-dynamic" :class="{ 'rotate-180': expanded }" />
+        </div>
+        <div v-if="expanded" class="mt-1">
+            {{ moreInfoText }}
+        </div>
     </div>
 </template>
