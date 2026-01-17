9a6229a228407897e617d7cbc2a6674d9d6482c5
ImDarkTom <darktomg21@gmail.com>
2026-01-17 18:46:04 +0000

    chore: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default defineConfig({
-    test: {
-        environment: 'jsdom',
-        setupFiles: './tests/setup.ts',
-        coverage: {
-            exclude: [ 'dev-dist/', 'tailwind.config.js', 'eslint.config.mjs', 'vite.config.ts', 'dist/' ]
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            environment: 'jsdom',
+            setupFiles: './tests/setup.ts',
+            coverage: {
+                exclude: [ 'dev-dist/', 'tailwind.config.js', 'eslint.config.mjs', 'vite.config.ts', 'dist/' ]
+            },
         },
-    },
-});
+    }),
+);
