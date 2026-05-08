fe7b95380b02291922c81358667a7ed0a4b80b8e
ImDarkTom <darktomg21@gmail.com>
2026-05-08 01:42:21 +0100

    feat: use new shortcut renderer for shortcuts popup

--- src/components/Popup/Shortcuts/Display.vue
+++ src/components/Popup/Shortcuts/Display.vue
@@ -1,25 +1,17 @@
 <script setup lang="ts">
 defineProps<{
     label: string;
-    shortcut: string;
+    hotkey: Hotkey;
 }>();
-
-const getTitleCase = (text: string) => {
-    return text.charAt(0).toUpperCase() + text.slice(1);
-};
 </script>
 
 <template>
     <div class="flex flex-col">
         <div class="flex flex-row w-full items-center justify-between text-sm">
-            <span>{{ label }}</span>
-            <div class="flex flex-row gap-2">
-                <kbd 
-                    v-for="key in shortcut.split(' ')"
-                    class="bg-base-800 p-1 rounded-lg ring-1 ring-base-400 px-3 text-center">
-                    {{ getTitleCase(key) }}
-                </kbd>
-            </div>
+            <span class="font-medium">{{ label }}</span>
+            <UIKeyboardShortcutRenderer 
+                class="text-sm! text-base-300!" 
+                :hotkey />
         </div>
         <hr class="my-4 text-base-500">
     </div>
