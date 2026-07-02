3dfa3edb2f6de8801c0bcdcb6d17c815bea7e5d4
ImDarkTom <darktomg21@gmail.com>
2026-07-02 03:18:31 +0100

    fix(settings): toggle page shift

--- src/components/Settings/Input/Toggle.vue
+++ src/components/Settings/Input/Toggle.vue
@@ -1,42 +1,48 @@
 <script setup lang="ts">
+import { Label } from 'reka-ui';
+
 const emit = defineEmits(['update:modelValue']);
 
 const props = defineProps<{
-	modelValue: boolean;
-	label: string;
-	tooltip?: string;
+    modelValue: boolean;
+    label: string;
+    tooltip?: string;
 }>();
 
 const updateValue = () => {
-	emit('update:modelValue', !props.modelValue);
-}
+    emit('update:modelValue', !props.modelValue);
+};
+
+const id = useId();
 </script>
 
 <template>
-	<label class="w-full flex flex-row justify-between items-center transition-all duration-dynamic" @click="updateValue">
-		<SettingsOptionText :label :tooltip />
-		
-		<input 
-			class="sr-only"
-			type="checkbox"
-			:checked="modelValue"
-			@change="updateValue"
-			:aria-label="label || 'Toggle'"
-		/>
+    <div class="group relative w-full flex flex-row justify-between items-center transition-all duration-dynamic">
+        <Label :for="id">
+            <SettingsOptionText
+                :label
+                :tooltip />
+        </Label>
 
-		<div
-			class="w-12 h-6 flex items-center bg-base-950 rounded-full p-0.5 cursor-pointer transition-all duration-dynamic"
-			:class="{ 'bg-primary': modelValue }"
-			role="switch"
-			:aria-checked="modelValue"
-			tabindex="0"
-			@keydown.space.prevent="updateValue"
-		>
-			<div
-				class="size-5 bg-base-100 rounded-full transition-transform duration-dynamic"
-				:class="{ 'translate-x-6': modelValue }"
-			></div>
-		</div>
+        <input
+            type="checkbox"
+            class="absolute right-5 hidden"
+            :id
+            :aria-label="label"
+            :checked="modelValue"
+            @change="updateValue" />
 
-	</label>
+        <label
+            class="w-12 h-6 flex items-center bg-base-950 group-hover:bg-base-900 rounded-full p-0.5 cursor-pointer"
+            role="switch"
+            tabindex="0"
+            :class="{ 'bg-primary group-hover:bg-secondary': modelValue }"
+            :for="id"
+            :aria-checked="modelValue"
+            @keydown.space.prevent="updateValue">
+            <div
+                class="size-5 bg-base-100 rounded-full transition-transform duration-dynamic"
+                :class="{ 'translate-x-6': modelValue }"></div>
+        </label>
+    </div>
 </template>
